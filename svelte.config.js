45726883a5288c8d559b3a74a452134b76bbd3d0
Dinayker Lopez <dinaykerlopez5@gmail.com>
2026-03-10 17:55:35 +0000

    correcion al deploy.yml

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,22 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'; // Importante: debe ser el static
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html' 
-		}),
-		paths: {
-			// Si tu repo se llama "mi-proyecto", pon '/mi-proyecto'
-			// Si es tu sitio principal (nombre.github.io), deja la cadena vacía ''
-			base: process.env.NODE_ENV === 'production' ? 'https://dinaycrak.github.io/SGPA-frontend/' : '',
-		}
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        // AQUÍ es donde va el bloque del adapter
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            strict: false 
+        }),
+        paths: {
+            // Esto asegura que los estilos carguen bien en GitHub
+            base: process.env.NODE_ENV === 'production' ? '/SGPA-frontend' : '',
+        }
+    }
 };
 
 export default config;
